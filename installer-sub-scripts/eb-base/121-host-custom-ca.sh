4bc7ebf1b2c9490bd589e9a4dfa6bf0b4321b4c1
thcerit <36306750+thcerit@users.noreply.github.com>
2024-09-04 16:32:09 +0300

    Update 121-host-custom-ca.sh

--- installer-sub-scripts/eb-base/121-host-custom-ca.sh
+++ installer-sub-scripts/eb-base/121-host-custom-ca.sh
@@ -26,7 +26,7 @@ echo "---------------------- HOST CUSTOM CA ---------------------"
 export DEBIAN_FRONTEND=noninteractive
 
 # added packages
-apt-get $APT_PROXY -y install openssl
+dnf $APT_PROXY -y install openssl
 
 # ------------------------------------------------------------------------------
 # CA CERTIFICATE & KEY
